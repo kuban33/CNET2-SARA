684af05f5c619b43981857124872771364d90a15
kubicek-skoleni <kubicek.brno@gmail.com>
2024-05-27 11:30:11 +0200

    konstruktory a instance

---
+++ CNET2/ConsoleApp/Animal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp;
+
+public class Animal
+{
+    private string _name;
+
+    public Animal()
+    {
+
+    }
+    public Animal(string name, Species species)
+    {
+        Name = name;
+        Species = species;
+    }
+
+    public string Name { get; set; }
+
+
+    public Species Species { get; set; }
+
+}
+
+public enum Species
+{
+    CAT,
+    DOG
+}
+
--- CNET2/ConsoleApp/Program.cs
+++ CNET2/ConsoleApp/Program.cs
@@ -1,38 +1,71 @@
-﻿// Lukáš, Karel, Tomáš, Martin, Radim, Jakub, Sofián
+﻿// třída pro zvíře
+//  * druh
+//  * jméno
+// vytvořit několik instancí
+// vypsat do konzole
 
+using ConsoleApp;
+
+Animal zvire1 = new Animal("Mourek", Species.CAT);
+Animal zvire2 = new Animal();
+//Animal zvire3 = AnimalLibrary.Get();
+
+zvire2.Name = "Alík";
+zvire2.Species = Species.DOG;
+
+
+Animal zvire3 = new("Mourek", Species.CAT);
+
+var zvire4 = new Animal();
+//zvire4 = "hello"; - chyba
+
+// var zvire5 = new(); - nedava smysl
+
+
+
+
+
+
+
+
+
+
+
+
+// Lukáš, Karel, Tomáš, Martin, Radim, Jakub, Sofián
 // zeptat se na jméno a pozdravit
 // nebo říct že neznám
 
-Console.WriteLine("Ahoj, zadej své jméno:");
+//Console.WriteLine("Ahoj, zadej své jméno:");
 
-string jmeno = Console.ReadLine();
+//string jmeno = Console.ReadLine();
 
-switch (jmeno)
-{
-    case "Lukáš":
-        Console.WriteLine("Ahoj Lukáši");
-        break;
-    case "Karel":
-        Console.WriteLine("Ahoj Karle");
-        break;
-    case "Tomáš":
-        Console.WriteLine("Ahoj Tomáši");
-        break;
-    case "Martin":
-        Console.WriteLine("Ahoj Martine");
-        break;
-    case "Radim":
-        Console.WriteLine("Ahoj Radime");
-        break;
-    case "Jakub":
-        Console.WriteLine("Ahoj Jakube");
-        break;
-    case "Sofián":
-        Console.WriteLine("Ahoj Sofiáne");
-        break;
-    default:
-        Console.WriteLine("Takové jméno neznám.");
-        break;
-}
+//switch (jmeno)
+//{
+//    case "Lukáš":
+//        Console.WriteLine("Ahoj Lukáši");
+//        break;
+//    case "Karel":
+//        Console.WriteLine("Ahoj Karle");
+//        break;
+//    case "Tomáš":
+//        Console.WriteLine("Ahoj Tomáši");
+//        break;
+//    case "Martin":
+//        Console.WriteLine("Ahoj Martine");
+//        break;
+//    case "Radim":
+//        Console.WriteLine("Ahoj Radime");
+//        break;
+//    case "Jakub":
+//        Console.WriteLine("Ahoj Jakube");
+//        break;
+//    case "Sofián":
+//        Console.WriteLine("Ahoj Sofiáne");
+//        break;
+//    default:
+//        Console.WriteLine("Takové jméno neznám.");
+//        break;
+//}
 
 
