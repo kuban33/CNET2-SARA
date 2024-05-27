18f5c86fd1e9d39f9f72d6f87003b33bcdc67c5b
kuban33 <kuban33@email.cz>
2024-05-27 13:47:16 +0200

    jmena osloveni dictionary

--- CNET2/ConsoleApp/Program.cs
+++ CNET2/ConsoleApp/Program.cs
@@ -12,9 +12,29 @@ List<string> jmena_list = ["Lukáš", "Karel", "Tomáš",
 List<string> jmena_list2 = new() {"Lukáš", "Karel", "Tomáš",
             "Martin", "Radim", "Jakub", "Sofián"};
 
-foreach(string jmeno in jmena_list)
+//foreach(string jmeno in jmena_list)
+//{
+//    Console.WriteLine(jmeno);
+//}
+
+Dictionary<string, string> jmenaOsloveni = new Dictionary<string, string>()
 {
-    Console.WriteLine(jmeno);
+    { "Lukas", "Lukasi" },
+    { "Karel", "Karle" },
+    { "Tomas", "Tomasi" },
+};
+
+Console.WriteLine("Ahoj, zadej své jméno:");
+
+string jmeno = Console.ReadLine();
+
+if (jmenaOsloveni.ContainsKey(jmeno))
+{
+    Console.WriteLine($"Ahoj {jmenaOsloveni[jmeno]}.");
+}
+else
+{
+    Console.WriteLine("neznam");
 }
 
 //for(int i = 0; i < jmena.Length; i++)
