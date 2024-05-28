a24e9d24a08bba584b1fcb19637ee1de5a2da533
kuban33 <kuban33@email.cz>
2024-05-28 14:15:23 +0200

    ASP PersonAPI

---
+++ CNET2/PersonAPI/Program.cs
@@ -0,0 +1,21 @@
+using PersonData;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<PeopleContext>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.MapGet("/person/{id}", (int id, PeopleContext db) =>
+                            db.People.Where(x => x.Id == id).Single());
+
+
+app.Run();
+
+
