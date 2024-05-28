b1d1441fd907ec1509dc57dc9e5999e1a3a7c7ff
kuban33 <kuban33@email.cz>
2024-05-28 14:20:55 +0200

    email ukol

--- CNET2/PersonAPI/Program.cs
+++ CNET2/PersonAPI/Program.cs
@@ -15,6 +15,8 @@ app.UseHttpsRedirection();
 app.MapGet("/person/{id}", (int id, PeopleContext db) =>
                             db.People.Where(x => x.Id == id).Single());
 
+app.MapGet("/emailsearch/{searchstr}", (string searchstr, PeopleContext db) =>
+                            db.People.Where(x => x.Email.Contains(searchstr)));
 
 app.Run();
 
